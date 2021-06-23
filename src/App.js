8d211fcfaa53bd57242eac93ff3dd72ef25ef6ac
Nichlas <nichlasd@gmail.com>
2021-06-23 14:08:30 +0200

    add a way to see the manager account with how many players and amount of ether

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import web3 from "./web3";
+import lottery from './lottery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+   state = {
+       manager: '',
+       players: [],
+       balance: ''
+   };
+
+    async componentDidMount() {
+        const manager = await lottery.methods.manager().call();
+        const players = await lottery.methods.getPlayers().call();
+        const balance = await web3.eth.getBalance(lottery.options.address);
+        
+    
+        this.setState({manager, players, balance});
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Lotter Contract</h2>
+                <p>This contract is managed by {this.state.manager}</p>
+                <p>There are currently {this.state.players.length} people entered,
+                    competeting to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!</p>
+                
+            </div>
+        );
+    }
 }
 
 export default App;
